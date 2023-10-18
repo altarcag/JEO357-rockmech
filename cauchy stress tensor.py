d182bd7b9c693bf3ce8bf18614037bd271fe704d
Taha Altar Çağ <altarcag@gmail.com>
2023-10-18 08:53:55 +0300

    Update cauchy stress tensor.py

--- cauchy stress tensor.py
+++ cauchy stress tensor.py
@@ -1,30 +1,31 @@
-#rock mechanics
+# rock mechanics
 
 import math
 from random import randrange
 
-sigma_x = randrange(0, 50)
- 
+sigma_x = randrange(-50, 50)
+
 print("sigma x =", sigma_x)
 
-sigma_y = randrange(0, 50)
+sigma_y = randrange(-50, 50)
 
 print("sigma y =", sigma_y)
 
-tau_xy = randrange(0, 20)
+tau_xy = randrange(-20, 20)
 
 print("tau xy =", tau_xy)
 
-theta = randrange(1, 10) * 5
+theta = randrange(1, 12) * 5
 twotheta = theta * 2
 
 print("theta =", theta)
 print("2 * theta = ", twotheta)
 
-sigma_theta = sigma_x * math.cos(math.radians(theta))**2 + sigma_y * math.sin(math.radians(theta))**2 + tau_xy * math.sin(math.radians(twotheta))
+sigma_theta = sigma_x * math.cos(math.radians(theta)) ** 2 + sigma_y * math.sin(
+    math.radians(theta)) ** 2 + tau_xy * math.sin(math.radians(twotheta))
 
 print("sigma theta =", sigma_theta)
 
-tau_theta = ((sigma_y - sigma_x)/2) * math.sin(math.radians(twotheta)) + tau_xy * math.cos(math.radians(twotheta))
+tau_theta = ((sigma_y - sigma_x) / 2) * math.sin(math.radians(twotheta)) + tau_xy * math.cos(math.radians(twotheta))
 
 print("Tau theta =", tau_theta)
