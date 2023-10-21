630a86919140d5a2a68333affa3818398dadb23b
Taha Altar Çağ <altarcag@gmail.com>
2023-10-21 22:51:21 +0300

    Update cauchy stress tensor.py

--- cauchy stress tensor.py
+++ cauchy stress tensor.py
@@ -1,31 +1,68 @@
-# rock mechanics
+#rock mechanics
 
 import math
 from random import randrange
 
-sigma_x = randrange(-50, 50)
 
-print("sigma x =", sigma_x)
+def stress_tensor(sigma_x, sigma_y, tau_xy, theta, twotheta):
+        
+    sigma_theta = sigma_x * math.cos(math.radians(theta))**2 + sigma_y * math.sin(math.radians(theta))**2 + tau_xy * math.sin(math.radians(twotheta))
+    print("sigma theta =", sigma_theta)
 
-sigma_y = randrange(-50, 50)
+    sigma_y_prime = sigma_x * math.sin(math.radians(theta))**2 + sigma_y * math.cos(math.radians(theta))**2 - tau_xy * math.sin(math.radians(twotheta))
+    print("sigma y' =", sigma_y_prime)
 
-print("sigma y =", sigma_y)
+    tau_theta = ((sigma_y - sigma_x)/2) * math.sin(math.radians(twotheta)) + tau_xy * math.cos(math.radians(twotheta))
+    print("Tau theta =", tau_theta)
 
-tau_xy = randrange(-20, 20)
+    return {
+        "sigma_theta": sigma_theta,
+        "sigma_y_prime": sigma_y_prime,
+        "tau_theta": tau_theta
+    }
 
-print("tau xy =", tau_xy)
 
-theta = randrange(1, 12) * 5
-twotheta = theta * 2
+def generator(sigma_x, sigma_y, tau_xy, theta, twotheta):
+    
+    print("sigma x =", sigma_x)
+    print("sigma y =", sigma_y)
+    print("tau xy =", tau_xy)
+    print("theta =", theta)
+    print("2 * theta = ", twotheta)
 
-print("theta =", theta)
-print("2 * theta = ", twotheta)
+    result = stress_tensor(sigma_x, sigma_y, tau_xy, theta, twotheta)
+    return result
 
-sigma_theta = sigma_x * math.cos(math.radians(theta)) ** 2 + sigma_y * math.sin(
-    math.radians(theta)) ** 2 + tau_xy * math.sin(math.radians(twotheta))
 
-print("sigma theta =", sigma_theta)
+print("Select operation.")
+print("1. question generator")
+print("2. stress tensor by inputs")
 
-tau_theta = ((sigma_y - sigma_x) / 2) * math.sin(math.radians(twotheta)) + tau_xy * math.cos(math.radians(twotheta))
+while True:
+    #taking input
+    choice = input("Enter choice (1/2): ")
 
-print("Tau theta =", tau_theta)
+    #checking the choice
+    if choice == '1':
+        sigma_x = randrange(0, 50)
+        sigma_y = randrange(0, 50)
+        tau_xy = randrange(0, 20)
+        theta = randrange(1, 10) * 5
+        twotheta = theta * 2
+        qa = generator(sigma_x, sigma_y, tau_xy, theta, twotheta)
+        print(qa)
+        
+
+    elif choice == '2':
+        sigma_x = float(input("Enter sigma_x: "))
+        sigma_y = float(input("Enter sigma_y: "))
+        tau_xy = float(input("Enter tau_xy: "))
+        theta = float(input("Enter theta: "))
+        twotheta = theta * 2
+
+        stress_tensor(sigma_x, sigma_y, tau_xy, theta, twotheta)
+        print(stress_tensor)
+
+    else:
+        print("invalid input")
+        break
